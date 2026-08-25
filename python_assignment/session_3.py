53e8caafee8cae409e85bc177447a9a03bdae239
kapil kumar <kkapil9554@gmail.com>
2026-08-25 22:12:05 +0530

    added sesion_3 file

--- python_assignment/session_3.py
+++ python_assignment/session_3.py
@@ -1,8 +1,8 @@
 # task 1
 
-# name=(input("enter your name:"))
-# food=(input("enter your favorite food:"))
-# print("hello" ",",name ,"your favorite food is",food +"!")
+name=(input("enter your name:"))
+food=(input("enter your favorite food:"))
+print("hello",name,"," ,"your favorite food is",food +"!")
 
 # task 2
 
@@ -14,4 +14,35 @@ print("product",num1*num2)
 print("quotient",num1/num2)
 
 # task 3
+price=float(input("enter food price:"))
+quantity=int(input("enter fodd quantity:"))
+total_bill=price*quantity
+print("your total_bill is =" +format(total_bill, ".2f"))
+
+# task 4 instagram followers
+
+followers=int(input("enter your instagram followers count:"))
+print("\n\tyou have",format(followers,"d"),"followers")
+
+# task 5 basic calculator
+
+number1=float(input("enter first number:"))
+number2=float(input("enter second number:"))
+operator=input("enter operator (+,_,/,*)")
+if operator=="+":
+    Result=number1+number2
+    print("Result",Result)
+elif operator=="_":
+    Result=number1-number2
+    print("Result",Result)
+elif operator=="*":
+    Result=number1*number2
+    print("Result",Result)
+elif operator=="/":
+    Result=number1/number2
+    print("Result",Result)
+else:
+    print("enter valid operator")
+
+
 
