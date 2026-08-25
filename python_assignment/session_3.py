ea42c4a65a1e6e7f4c04fe5892483610c74328fe
kapil kumar <kkapil9554@gmail.com>
2026-08-25 09:57:22 +0530

    added sesion_3 file

---
+++ python_assignment/session_3.py
@@ -0,0 +1,17 @@
+# task 1
+
+# name=(input("enter your name:"))
+# food=(input("enter your favorite food:"))
+# print("hello" ",",name ,"your favorite food is",food +"!")
+
+# task 2
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+print("sum",num1+num2)
+print("difference",num1-num2)
+print("product",num1*num2)
+print("quotient",num1/num2)
+
+# task 3
+
